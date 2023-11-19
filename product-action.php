efbcc6d0cd9fc4ed3681317150b8feb8a718f821
Shreyas KB <96857515+shreyaskbkukke@users.noreply.github.com>
2023-11-19 11:09:07 +0530

    Update product-action.php

--- product-action.php
+++ product-action.php
@@ -1,9 +1,3 @@
-<!--  Author Name: MH RONY.
-                        GigHub Link: https://github.com/dev-mhrony
-                        Facebook Link:https://www.facebook.com/dev.mhrony
-                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
-                        Visit My Website : developerrony.com -->
 <?php
 if(!empty($_GET["action"])) 
 {
@@ -71,10 +65,3 @@ switch($_GET["action"])
 
 
 ?>
-
-<!--  Author Name: MH RONY.
-                        GigHub Link: https://github.com/dev-mhrony
-                        Facebook Link:https://www.facebook.com/dev.mhrony
-                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
-                        Visit My Website : developerrony.com -->
