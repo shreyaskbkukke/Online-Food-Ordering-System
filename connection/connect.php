6b473fc1e93ea93f9053c672dda45a4b954a35f7
Shreyas KB <96857515+shreyaskbkukke@users.noreply.github.com>
2023-11-19 11:40:11 +0530

    Update connect.php

--- connection/connect.php
+++ connection/connect.php
@@ -1,13 +1,4 @@
 <?php
-/*!
- *  Author Name: MH RONY.
- *  GigHub Link: https://github.com/dev-mhrony
- *  Facebook Link:https://www.facebook.com/dev.mhrony
- *  Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-    for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
- *  Visit My Website : developerrony.com 
-
-      */
 //main connection file for both admin & front end
 $servername = "localhost"; //server
 $username = "root"; //username
@@ -20,13 +11,4 @@ $db = mysqli_connect($servername, $username, $password, $dbname); // connecting
 if (!$db) {       //checking connection to DB	
     die("Connection failed: " . mysqli_connect_error());
 }
-/*!
- *  Author Name: MH RONY.
- *  GigHub Link: https://github.com/dev-mhrony
- *  Facebook Link:https://www.facebook.com/dev.mhrony
- *  Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-    for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
- *  Visit My Website : developerrony.com 
-
-      */
 ?>
